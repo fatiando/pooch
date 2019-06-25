70931943db49916a52167ca1411a661be9481966
Leonardo Uieda <leouieda@gmail.com>
2019-06-24 16:02:42 -1000

    Add 3.5 compat metadata

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ CLASSIFIERS = [
     "Topic :: Scientific/Engineering",
     "Topic :: Software Development :: Libraries",
     "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "License :: OSI Approved :: {}".format(LICENSE),
