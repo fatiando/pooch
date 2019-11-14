c64d5a1d9e1f04301f6aab18b1a7c71d08276afe
Hugo van Kemenade <hugovk@users.noreply.github.com>
2019-11-14 12:16:45 +0200

    Add CI jobs and setup.py flags for Python 3.8 (#113)
    Run the tests and make sure development works on 3.8.
    Keep the environment.yml as 3.7 still.

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3 :: Only",
     "License :: OSI Approved :: {}".format(LICENSE),
 ]
