d7016ddd9e8b6c39f16cbec6653f6cd66c75bc90
Daniel Shapero <shapero.daniel@gmail.com>
2020-01-20 15:20:17 -0800

    Better docstring for get_logger

--- pooch/utils.py
+++ pooch/utils.py
@@ -14,7 +14,18 @@ LOGGER.addHandler(logging.StreamHandler())
 
 
 def get_logger():
-    r"""Return the object that logs events from pooch"""
+    r"""
+    Get the default event logger.
+
+    The logger records events like downloading files, unzipping archives, etc.
+    Use the method :meth:`logging.Logger.setLevel` of this object to adjust the
+    verbosity level from Pooch.
+
+    Returns
+    -------
+    logger : :class:`logging.Logger`
+        The logger object for Pooch
+    """
     return LOGGER
 
 
