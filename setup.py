cdf35b7ddd635390973637bfaeed049c97dc0f78
Leonardo Uieda <leouieda@gmail.com>
2020-11-27 07:31:43 +0000

    Add support and testing for Python 3.9
    Run tests on CI to verify that things work with the new release.

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3 :: Only",
     f"License :: OSI Approved :: {LICENSE}",
 ]
