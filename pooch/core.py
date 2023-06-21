bc7632bddde96d2ca12868186b280022a4e5b23a
Leonardo Uieda <leouieda@gmail.com>
2023-06-21 11:00:11 -0300

    Update pooch/core.py

--- pooch/core.py
+++ pooch/core.py
@@ -755,6 +755,7 @@ def stream_download(url, fname, known_hash, downloader, pooch=None, retry_if_fai
     will retry the download the specified number of times in case the failure
     was due to a network error.
     """
+    # Lazy import requests to speed up import time
     import requests.exceptions  # pylint: disable=C0415
 
     # Ensure the parent directory exists in case the file is in a subdirectory.
